3db9bf961772c9102619745aafd485783fdd6ff2
Kim Jun Ha <junhaha3@gamil.com>
2021-10-01 23:48:34 +0900

    블럭 하강, 게임 종료 조건 완성

--- tetris/src/tetris/TetrisGameView.java
+++ tetris/src/tetris/TetrisGameView.java
@@ -17,7 +17,7 @@ public class TetrisGameView extends JPanel{
 	int randomC = (int) (Math.random() * 4);
 	
 	//블럭 높이, 좌표
-	int hgt = 0;
+	int hgt = -20;
 	int wid  = 100;
 	int[] curX =  new int[4];
 	int[] curY =  new int[4];
@@ -36,19 +36,19 @@ public class TetrisGameView extends JPanel{
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		paintBorder(g);
-		lookBlock(g);
-		drawBoard(g);
-		findCoord();
-		blockDown(g);
+		paintBorder(g); // 테두리 그리기
+		drawBoard(g); //보드판 그리기
+		lookBlock(g); // 블럭 미리보기
 		
+		endJudge(); //게임 종료 판정
+		findCoord(); // 생성된 블럭 좌표 구하기
+		drawBlock(g); // 생성된 블럭 그리기
 		
-		blockToWall();
+		blockToWall(); //벽이나 바닥 만나면 벽으로 바뀜. 
 		if(End) {
 			changeRandom();
 			End = false;
 		}
-		
 	}
 	
 	// 블럭 미리 보기
@@ -64,7 +64,7 @@ public class TetrisGameView extends JPanel{
 	}
 	
 	//블럭 좌표 구하기
-	public void findCoord() { //1차 완성
+	public void findCoord() { //완성
 		int cnt = 0;
 		for (int i = 0; i<4; i++) {
 			for (int j = 0; j < 4; j++) {
@@ -77,54 +77,52 @@ public class TetrisGameView extends JPanel{
 		}
 	}
 	
-	//블럭 그리기
-	public void blockDown(Graphics g) { //미완성 색깔 이상
+	//블록 그리기
+	public void drawBlock(Graphics g) { //완성
 		g.setColor(color[randomC]);
-		for (int i = 0; i<4; i++) {
-			for (int j = 0; j < 4; j++) {
-				if(b.block[randomB][b.rotate][i][j] == 1) {
-					g.draw3DRect(j * b.size + wid, i * b.size + hgt, b.size, b.size, true);
-				}
-			}
+		for (int i = 0; i < 4; i++) {
+			g.fill3DRect(curX[i] * b.size, curY[i] * b.size, b.size, b.size, true);
 		}
 	}
 	
 	//보드 그리기
-	public void drawBoard(Graphics g) { //미완성 색깔 이상
-		g.setColor(Color.gray);
+	public void drawBoard(Graphics g) { //완성
 		for (int j = 0; j < 20; j++) {
 			for(int i = 1; i < 11; i++) {
-				if(b2.board[j][i] == 0)
-					g.draw3DRect((i-1) * b.size, j * b.size, b.size, b.size, true);
+				if(b2.board[j][i] == 1)
+					g.setColor(Color.gray);
+				else
+					g.setColor(Color.white);
+				g.fill3DRect((i-1) * b.size, j * b.size, b.size, b.size, true);
 			}
 		}
 	}
 	
 	
 	//블럭이 바닥이나 벽에 닿으면 벽
-	public void blockToWall() { // 미완성
+	public void blockToWall() { // 1차 완성
 		for (int i = 0; i < 4; i++) {
-			if(b2.board[curY[i] + 1][curX[i]] == 1) {
+			if(b2.board[curY[i] + 1][curX[i] + 1] == 1) { //x의 범위가 1~10까지여서 1을 더해줌. 
 				for (int j = 0; j < 4; j++) {
-					b2.board[curY[j]][curX[j]] = 1;
+					b2.board[curY[j]][curX[j] + 1] = 1;
 					wid = 100;
-					hgt = 0;
+					hgt = -20;
 					End = true;
 				}
 			}
 		}
 	}
 	
+	//게임 종료 조건 => 두번째 줄에 1이 있으면 게임 오버
+	public void endJudge() {
+		for(int i = 1; i< 11; i++) 
+			if (b2.board[1][i] == 1)
+				limit = false;
+	}
+	
 	//테트리스 테두리
 	public void paintBorder(Graphics g) { //1차 완성
 		g.setColor(Color.black);
-		
-		//테트리스 게임창
-		g.drawLine(0, 0, 199, 0);
-		g.drawLine(0, 0, 0, 399);
-		g.drawLine(199, 0, 199, 399);
-		g.drawLine(0, 399, 199, 399);
-		
 		//블럭 미리보기
 		g.drawLine(249, 0, 399, 0);
 		g.drawLine(249, 149, 399, 149);
